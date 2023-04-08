8c3a4fe09eacc773f8147f48b34491f44ab90430
Dawid Pura <dawid.pura@pw.edu.pl>
2023-04-08 14:07:27 +0200

    Adding more options to logging classes

--- server/src/logger.cpp
+++ server/src/logger.cpp
@@ -4,31 +4,70 @@
 
 using namespace Logging;
 
-void StdoutLogger::write(std::string level, std::string message)
+void BaseLogger::write(std::string level, std::string message)
 {
-    std::cout << this->name << " [" << level << "]: " << message << std::endl;
+    time_t now = time(0);
+    std::string timeStr = asctime(gmtime(&now));
+    timeStr.pop_back(); // a hack - asctime() always returns a string with an endline
+    this->write(timeStr + ": " + this->name + " [" + level + "]: " + message + "\n");
 }
 
-StdoutLogger::StdoutLogger(std::string name) : name(name)
+void BaseLogger::debug(std::string message)
 {
+    if (level == DEBUG)
+        this->write("debug", message);
 }
 
-void StdoutLogger::debug(std::string message)
+void BaseLogger::warn(std::string message)
 {
-    this->write("debug", message);
+    if (level <= WARN)
+        this->write("warn", message);
 }
 
-void StdoutLogger::warn(std::string message)
+void BaseLogger::error(std::string message)
 {
-    this->write("warn", message);
+    if (level <= ERROR)
+        this->write("error", message);
 }
 
-void StdoutLogger::error(std::string message)
+Logging::BaseLogger::BaseLogger(LogLevel level, std::string name) : name(name), level(level) { }
+
+BaseLoggerFactory::BaseLoggerFactory(LogLevel level) : level(level) { };
+
+void Logging::StdoutLogger::write(std::string entry)
 {
-    this->write("error", message);
+    std::cout << entry;
 }
+
+StdoutLogger::StdoutLogger(LogLevel level, std::string name) : BaseLogger(level, name) {}
 
 Logger& StdoutLoggerFactory::getLogger(std::string name)
 {
-    return *(new StdoutLogger(name));
+    return *(new StdoutLogger(this->level, name));
+}
+
+StdoutLoggerFactory::StdoutLoggerFactory(LogLevel level): BaseLoggerFactory(level) {}; 
+
+LoggerFactory &Logging::LoggerLoader::load()
+{
+    LogLevel l = DEBUG;
+    std::string s(getenv("SERVER_LOGGER"));
+    LoggerFactory* result = NULL;
+    if (s == "file") {
+
+    } else if (s == "stdout") {
+        result = new StdoutLoggerFactory(l);
+    }
+    return *(result);
+}
+
+Logging::FileLoggerFactory::FileLoggerFactory(LogLevel level, std::string filename) : BaseLoggerFactory(level), filename(filename) { }
+
+Logger &Logging::FileLoggerFactory::getLogger(std::string name)
+{
+    return *(new FileLogger(this->level, this->filename));
+}
+
+Logging::FileLogger::FileLogger(LogLevel level, std::string name) : BaseLogger(level, name)
+{
 }
--- server/src/logger.h
+++ server/src/logger.h
@@ -5,6 +5,18 @@
 
 namespace Logging {
 
+    enum LogLevel {
+        DEBUG, WARN, ERROR
+    };
+
+    class ConfigSource {
+
+    };
+
+    class Configuration {
+        LogLevel logLevel();
+    };
+
     class Logger {
     public:
         virtual void debug(std::string message) = 0;
@@ -12,29 +24,67 @@ namespace Logging {
         virtual void error(std::string message) = 0;
     };
 
-    class LoggerFactory {
-    public:
-        virtual Logger& getLogger(std::string name) = 0;
-    };
-
-    class StdoutLogger : public Logger {
+    /// @brief abstract class of basic logger - responsible for formatting and sending that information to abstrac write() method
+    class BaseLogger : public Logger {
     private:
+        LogLevel level;
         std::string name;
         void write(std::string level, std::string message);
+    protected:
+        BaseLogger(LogLevel level, std::string name);
+        virtual void write(std::string entry) = 0;
     public:
-        StdoutLogger(std::string name);
         void debug(std::string message);
         void warn(std::string message);
         void error(std::string message);
     };
 
-    class StdoutLoggerFactory : public LoggerFactory {
+    class LoggerFactory {
     public:
+        virtual Logger& getLogger(std::string name) = 0;
+    };
+
+    class BaseLoggerFactory : public LoggerFactory {
+    protected:
+        LogLevel level;
+    public:
+        BaseLoggerFactory(LogLevel level);
+    };
+
+    class StdoutLogger : public BaseLogger {
+    protected:
+        void write(std::string entry);
+    public:
+        StdoutLogger(LogLevel level, std::string name);
+    };
+
+    class StdoutLoggerFactory : public BaseLoggerFactory {
+    public:
+        StdoutLoggerFactory(LogLevel level);
         Logger& getLogger(std::string name);
     };
 
-    static StdoutLoggerFactory slf;
-    static LoggerFactory& LOG = static_cast<LoggerFactory&>(slf);
+    class FileLogger : public BaseLogger {
+    protected:
+        void write(std::string entry);
+    public:
+        FileLogger(LogLevel level, std::string name);
+    };
+
+    class FileLoggerFactory : public BaseLoggerFactory {
+    private:
+        std::string filename;
+    public:
+        FileLoggerFactory(LogLevel level, std::string filename);
+        Logger& getLogger(std::string name);
+    };
+
+    class LoggerLoader {
+    public:
+        static LoggerFactory& load();
+    };
+
+    static LoggerFactory& LOG = LoggerLoader::load();
 }
 
 #endif
